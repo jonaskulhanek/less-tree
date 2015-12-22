fa25cb96122a4df180646a44988e257bbd836f1c
tianxiang.wly <tianxiang.wly@alibaba-inc.com>
2015-12-22 23:06:26 +0800

    :smile:

--- test/test.js
+++ test/test.js
@@ -8,7 +8,22 @@ const CWD = process.cwd();
 describe('less-tree', () => {
   before(() => process.chdir(path.join(__dirname, 'source')));
   after(() => process.chdir(CWD));
-  it('', () => {
+  it('should to tree object', () => {
+    tree(path.join(process.cwd(), 'a.less')).toTreeObject().should.be.eql({
+      'b.less': {
+        'd.less': {
+          'sub/f.less': {}
+        }
+      },
+      'c.less': {
+        'd.less': {
+          'sub/f.less': {}
+        }
+      },
+      'sub/e.less': {}
+    });
+  });
+  it('should to tree string', () => {
     tree(path.join(process.cwd(), 'a.less')).toTreeString().trim().should.be.eql(`
 ├─ b.less
 │  └─ d.less
